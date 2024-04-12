6603b982bb20d977306e753bec39836424772816
Dawid64 <dawid.siera@gmail.com>
2024-04-12 23:04:38 +0200

    added PCAExtractor and __init__ update

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -2,3 +2,5 @@
 The :mod:`preprocessing` module includes tools for data preprecessing
 as well asmodule for benchmarking them.
 """
+from .extractor import PCAExtractor
+from .preprocessing import Preproccessing
