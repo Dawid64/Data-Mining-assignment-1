dc605382365e2710ccb46d0c6b0858fcefe4ccac
Marcin Leszczyński <venkman2003@gmail.com>
2024-04-22 10:35:18 +0200

    fixed path for data

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -2,9 +2,13 @@ from preprocessing.preprocessing import Preprocessing
 import pandas as pd
 
 def test_preprocessing():
-    path_to_data = 'C:/Users/Admin/Desktop/Data-Mining-assignment-1/spaceship-titanic/train.csv'
+    path_to_data = '../Data-Mining-assignment-1/spaceship-titanic/train.csv'
     preprocessor = Preprocessing(path=path_to_data, target='Transported')
     preprocessed_data = preprocessor.preprocess()
     #print(pd.read_csv(path_to_data))
     #print(preprocessed_data)
     assert(isinstance(preprocessed_data, pd.DataFrame))
+
+if __name__ == '__main__':
+    test_preprocessing()
+    print('success!!!')
