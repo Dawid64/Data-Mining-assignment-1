3f3f3f9ad9c21f95bb674cd39727f3100572f1ee
Dawid64 <dawid.siera@gmail.com>
2024-04-24 19:17:26 +0200

    documentation for benchmarks subpackage

--- preprocessing/benchmarks/__init__.py
+++ preprocessing/benchmarks/__init__.py
@@ -1,6 +1,17 @@
 """
-The :mod:`preprocessing.benchmarks` module includes few benchmarks for testing either
+The :mod:`preprocessing.benchmarks` subpackage includes several benchmarks for testing either
 the data preprocessing gives promissing results or not.
+
+Curently implemented benchmarks:
+- ClassifierBenchmark - Simple classification algorithm.
+- DNNBenchmark - Deep Neural Network algorithm.
+- SOMBenchmark - Self-Organizing tree algorithm.
+
+## Usage:
+>>> from preprocessing.benchmarks import DNNBenchmark
+>>> dataset = pd.read_csv('your_dataset.csv')
+>>> dnn = DNNBenchmark()
+>>> dnn.evaluate(dataset, target='Your Target')
 """
 from .classifier import ClassifierBenchmark
 from .dnn import DNNBenchmark
