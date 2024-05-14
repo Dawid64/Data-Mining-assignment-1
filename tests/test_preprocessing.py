f19b115b1cfcb35aba20e3c818b9e5865c5d3dd4
Dawid64 <dawid.siera@gmail.com>
2024-05-14 13:17:34 +0200

    Add tests for apply

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 from preprocessing.preprocessing import Preprocessing
-from preprocessing.selector import Selector
-from preprocessing.extractor import Extractor
+from preprocessing.selector import Selector, VARSelector
+from preprocessing.extractor import Extractor, PCAExtractor
 
 
 def test_preprocessing():
@@ -25,3 +25,28 @@ def test_preprocessing():
     assert pd.DataFrame(new_data).equals(preprocessing.dataset)
     preprocessing._na_handling()
     assert None not in preprocessing.dataset['d']
+
+
+def test_preprocessing_apply():
+    train_data = {'a': [1, 2, 3, 4], 'b': [5, 6, 7, 8],
+                  'c': [1, 1, 1, 1], 'd': [13, 14, None, 16],
+                  'e': ['A', 'B', 'A', 'B'], 'target': [0, 0, 1, 1]}
+
+    test_data = {'a': [1, 1, 1, 1, 1, 1], 'b': [5, 6, None, None, 2, 3],
+                 'c': [9, 10, 11, 12, 0, 0], 'd': [13, 14, 2, None, 12, 5],
+                 'e': ['B', 'B', 'B', 'B', 'D', 'E']}
+
+    train_data_frame = pd.DataFrame(data=train_data)
+    preprocessing = Preprocessing(
+        dataset=train_data_frame, target='target', one_hot_threshold=0.9,
+        selector=VARSelector(), extractor=PCAExtractor(num_components=1, target='target'))
+    train_result = preprocessing.preprocess()
+    assert isinstance(train_result, pd.DataFrame)
+    assert 'target' in train_result.columns
+    train_result.drop(columns=['target'], inplace=True)
+
+    test_data_frame = pd.DataFrame(data=test_data)
+    test_result = preprocessing.apply_preprocessing(test_data_frame)
+    assert isinstance(test_result, pd.DataFrame)
+    assert train_result.columns.equals(test_result.columns)
+    assert train_result.shape[1] == test_result.shape[1]
